4db55ec7ee3a4f7e3244fb918f2067986f7fbadc
Noah <noahcuroe@gmail.com>
2021-10-05 04:10:44 -0500

    Add Master Theorem Calculator

---
+++ MasterCalculator/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace MasterCalculator {
+    partial class FormMain {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.nudA = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nudB = new System.Windows.Forms.NumericUpDown();
+            this.nudC = new System.Windows.Forms.NumericUpDown();
+            this.nudD = new System.Windows.Forms.NumericUpDown();
+            this.nudDeg = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudD)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDeg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudA
+            // 
+            this.nudA.Location = new System.Drawing.Point(83, 14);
+            this.nudA.Name = "nudA";
+            this.nudA.Size = new System.Drawing.Size(120, 23);
+            this.nudA.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "const a";
+            // 
+            // label2
+            // 
+            this.label2.AccessibleRole = System.Windows.Forms.AccessibleRole.Grip;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "const b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "const c";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "const d";
+            // 
+            // nudB
+            // 
+            this.nudB.Location = new System.Drawing.Point(83, 43);
+            this.nudB.Name = "nudB";
+            this.nudB.Size = new System.Drawing.Size(120, 23);
+            this.nudB.TabIndex = 5;
+            // 
+            // nudC
+            // 
+            this.nudC.Location = new System.Drawing.Point(83, 72);
+            this.nudC.Name = "nudC";
+            this.nudC.Size = new System.Drawing.Size(120, 23);
+            this.nudC.TabIndex = 6;
+            // 
+            // nudD
+            // 
+            this.nudD.Location = new System.Drawing.Point(83, 101);
+            this.nudD.Name = "nudD";
+            this.nudD.Size = new System.Drawing.Size(120, 23);
+            this.nudD.TabIndex = 7;
+            // 
+            // nudDeg
+            // 
+            this.nudDeg.DecimalPlaces = 1;
+            this.nudDeg.Location = new System.Drawing.Point(83, 130);
+            this.nudDeg.Name = "nudDeg";
+            this.nudDeg.Size = new System.Drawing.Size(120, 23);
+            this.nudDeg.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "f(n) degree";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(12, 159);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(254, 23);
+            this.btnCalculate.TabIndex = 10;
+            this.btnCalculate.Text = "Validate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 209);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Result:";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(12, 227);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(254, 305);
+            this.txtResult.TabIndex = 12;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 544);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.nudDeg);
+            this.Controls.Add(this.nudD);
+            this.Controls.Add(this.nudC);
+            this.Controls.Add(this.nudB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudA);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "Master Theorem Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.nudA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudD)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDeg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown nudB;
+        private System.Windows.Forms.NumericUpDown nudC;
+        private System.Windows.Forms.NumericUpDown nudD;
+        private System.Windows.Forms.NumericUpDown nudDeg;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
